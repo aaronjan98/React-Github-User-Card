a510d46b1ae9b731722708908d6542c7129431ed
aaronjan <aaronjan98@gmail.com>
2020-01-14 13:41:43 -0800

    step 1: fetched user data

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello!</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    followers: []
+  };
+
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users/aaronjan98')
+      .then(res => {
+        console.log('User Info:', res.data);
+        this.setState({
+          users: res.data
+        });
+      })
+      .catch(err => console.log(err));
+      console.log('cDM is running');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello!</h1>
+
+        <div className="UserInfo">
+          <img  src={this.state.users.avatar_url} />
+          {this.state.users.login}
+          {console.log(Object.entries(this.state.users))}
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
